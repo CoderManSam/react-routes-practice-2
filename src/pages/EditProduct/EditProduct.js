3c32ddab03d2d41abd955d88e0c628253969a457
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-07-25 15:43:30 +0100

    adding exercise files

--- src/pages/EditProduct/EditProduct.js
+++ src/pages/EditProduct/EditProduct.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 
 function EditProductPage(props) {
 
@@ -13,6 +14,16 @@ function EditProductPage(props) {
   //you get the product data from the location. See
   //ViewProductPage.js to check
 
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state) {
+      const {product} = location.state;
+
+      setProductToUpdate(product)
+    }
+  }, [location]);
+
   function handleChange(event) {
     const name = event.target.name;
     const value = event.target.value;
@@ -24,17 +35,19 @@ function EditProductPage(props) {
     event.preventDefault();
   }
 
+  console.log("productToUpdate", productToUpdate)
+
   if (!productToUpdate) return <div>Loading...</div>;
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Product Name</label>
+      <label htmlFor="name">{productToUpdate.name}</label>
       <input
         type="text"
-        id="name"
-        name="name"
+        id={productToUpdate.name}
+        name={productToUpdate.name}
         onChange={handleChange}
-        value={productToUpdate.name}
+        value=""
       />
       <button type="submit">Edit</button>
     </form>
